b959f7ddee03db094a939a9e3aa5b6504e05aeec
Akhilesh <tech.kr.akhi@gmail.com>
2024-02-11 17:35:32 +0530

    ok

--- src/app/admin/add-sub-admin/add-sub-admin.component.ts
+++ src/app/admin/add-sub-admin/add-sub-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { CrudService } from 'src/app/servies/crud.service';
 
@@ -16,14 +17,13 @@ export class AddSubAdminComponent implements OnInit {
     private _crud: CrudService,
     private _routing: Router,
     private fb: FormBuilder,
-  
+    private dilog: MatDialogRef<AddSubAdminComponent>
   ) {
 
   }
 
   ngOnInit(): void {
     this.subAdmin = this.fb.group({
-      deg: ['', Validators.required],
       name: ['', Validators.required],
       username: ['', Validators.required],
       mobile: ['', Validators.required],
@@ -35,27 +35,34 @@ export class AddSubAdminComponent implements OnInit {
 
 
   OnSubmit() {
-    const subadmin = new FormData()
-    subadmin.append('deg', 'user'),
-      subadmin.append('name', this.subAdmin.get('name')?.value)
-    subadmin.append('username', this.subAdmin.get('username')?.value)
-    subadmin.append('password', this.subAdmin.get('password')?.value)
-    subadmin.append('mobile', this.subAdmin.get('mobile')?.value)
-    subadmin.append('status', this.subAdmin.get('status')?.value)
-    this._crud.add_sub_admin(subadmin).subscribe(
-      (res: any) => {
-        console.log(res);
-        if (res.success == 1) {
-          alert("data insert successfully");
-          this.subAdmin.reset()
-          
-        }
-      },
-      (error) => {
-        console.log(error);
+    if (!this.subAdmin.valid) {
+      alert('Plz fill all the filds ')
+      return
+    } else {
 
-      }
-    )
+      const subadmin = new FormData()
+      subadmin.append('deg', 'user'),
+        subadmin.append('name', this.subAdmin.get('name')?.value)
+      subadmin.append('username', this.subAdmin.get('username')?.value)
+      subadmin.append('password', this.subAdmin.get('password')?.value)
+      subadmin.append('mobile', this.subAdmin.get('mobile')?.value)
+      subadmin.append('status', this.subAdmin.get('status')?.value)
+      this._crud.add_sub_admin(subadmin).subscribe(
+        (res: any) => {
+          console.log(res);
+          if (res.success == 1) {
+            alert("data insert successfully");
+            this.subAdmin.reset()
+            this.dilog.close()
+          }
+        },
+        (error) => {
+          console.log(error);
+
+        }
+      )
+    }
+
 
 
   }
--- src/app/userform/userform.component.ts
+++ src/app/userform/userform.component.ts
@@ -9,6 +9,47 @@ import { CrudService } from '../servies/crud.service';
 })
 export class UserformComponent implements OnInit {
 
+  districtOptions = [
+    { value: 'अररिया', viewValue: 'अररिया' },
+    { value: 'अरवल', viewValue: 'अरवल' },
+    { value: 'औरंगाबाद', viewValue: 'औरंगाबाद' },
+    { value: 'बांका', viewValue: 'बांका' },
+    { value: 'बेगूसराय', viewValue: 'बेगूसराय' },
+    { value: 'भागलपुर', viewValue: 'भागलपुर' },
+    { value: 'भोजपुर', viewValue: 'भोजपुर' },
+    { value: 'बक्सर', viewValue: 'बक्सर' },
+    { value: 'दरभंगा', viewValue: 'दरभंगा' },
+    { value: 'गया', viewValue: 'गया' },
+    { value: 'गोपालगंज', viewValue: 'गोपालगंज' },
+    { value: 'जमुई', viewValue: 'जमुई' },
+    { value: 'जहानाबाद', viewValue: 'जहानाबाद' },
+    { value: 'कैमूर ( भभुआ )', viewValue: 'कैमूर ( भभुआ )' },
+    { value: 'कटिहार', viewValue: 'कटिहार' },
+    { value: 'खगड़िया', viewValue: 'खगड़िया' },
+    { value: 'किशनगंज', viewValue: 'किशनगंज' },
+    { value: 'लखीसराय', viewValue: 'लखीसराय' },
+    { value: 'मधेपुरा', viewValue: 'मधेपुरा' },
+    { value: 'मधुबनी', viewValue: 'मधुबनी' },
+    { value: 'मुंगेर', viewValue: 'मुंगेर' },
+    { value: 'मुजफ्फरपुर', viewValue: 'मुजफ्फरपुर' },
+    { value: 'नालंदा', viewValue: 'नालंदा' },
+    { value: 'नवादा', viewValue: 'नवादा' },
+    { value: 'पश्चिम चम्पारण', viewValue: 'पश्चिम चम्पारण' },
+    { value: 'पटना', viewValue: 'पटना' },
+    { value: 'पूर्वी चंपारण', viewValue: 'पूर्वी चंपारण' },
+    { value: 'पूर्णिया', viewValue: 'पूर्णिया' },
+    { value: 'रोहतास', viewValue: 'रोहतास' },
+    { value: 'सहरसा', viewValue: 'सहरसा' },
+    { value: 'समस्तीपुर', viewValue: 'समस्तीपुर' },
+    { value: 'सारण', viewValue: 'सारण' },
+    { value: 'शेखपुरा', viewValue: 'शेखपुरा' },
+    { value: 'शिवहर', viewValue: 'शिवहर' },
+    { value: 'सीतामढी', viewValue: 'सीतामढी' },
+    { value: 'सिवान', viewValue: 'सिवान' },
+    { value: 'सुपौल', viewValue: 'सुपौल' },
+    { value: 'वैशाली', viewValue: 'वैशाली' }
+  ];
+  
   userForm !: FormGroup
   profile_url: any = "../../assets/default_profile.png";
   profile_img: any;
@@ -37,7 +78,7 @@ export class UserformComponent implements OnInit {
       ps: ['', Validators.required],
       dist: ['', Validators.required],
       state: ['', Validators.required],
-      pincode: ['', Validators.required],
+      pincode: ['', Validators.required,Validators.max(6)],
     })
     this.send_mail()
 
@@ -72,6 +113,7 @@ export class UserformComponent implements OnInit {
         console.log(res);
         if (res.success == 1) {
           alert("data insert successfully");
+          this.userForm.reset()
         }
       },
       (error) => {
